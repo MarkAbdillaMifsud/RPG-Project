fbcea1d080d146a6509089855b14f82c8d967493
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-05-30 14:22:27 +0200

    Player moves towards mouse clicks through Raycasting

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour {
-    [SerializeField] Transform target;
 
     private void Update() {
-        GetComponent<NavMeshAgent>().destination = target.transform.position;
+        if (Input.GetMouseButtonDown(0)) {
+            MoveToCursor();
+        }
     }
     
+    private void MoveToCursor() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit) {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
 }
