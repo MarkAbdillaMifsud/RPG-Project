5b567281648af95e569ba5bda59a3d61a49a6363
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-07-04 15:03:58 +0200

    Refactoring into PlayerController

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -6,19 +6,12 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour {
 
     private void Update() {
-        if (Input.GetMouseButton(0)) {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
     
-    private void MoveToCursor() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit) {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+    public void MoveTo(Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     private void UpdateAnimator()
