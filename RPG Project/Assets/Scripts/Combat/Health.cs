8fbdf8248c0e8de34ef4b16a426fe59f2db4ff09
DESKTOP-G53QRMN\marka <markabdilla26@gmail.com>
2021-08-06 13:44:51 +0200

    Trigger a death animation

--- RPG Project/Assets/Scripts/Combat/Health.cs
+++ RPG Project/Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,27 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour 
     {
-        [SerializeField] float health = 100f;   
+        [SerializeField] float health = 100f;
+
+        bool isDead = false;   
 
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0); //take either the value of health - damage, or 0 is the previous falls into the negative
             print(health);
+            if (health == 0)
+            {
+                Die();
+            }
+        }
+
+        void Die()
+        {
+            if (!isDead)
+            {
+                GetComponent<Animator>().SetTrigger("die");
+                isDead = true;
+            }
         } 
     }
 }
